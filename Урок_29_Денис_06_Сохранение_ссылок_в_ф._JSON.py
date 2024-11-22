f1be03af7168fa2d870437d5554c4fce7acda966
Programmer <shakirov.ekb@gmail.com>
2024-11-22 22:56:40 +0500

    Добавлена функция сохранения в формате JSON.

---
+++ Урок_29_Денис_06_Сохранение_ссылок_в_ф._JSON.py
@@ -0,0 +1,50 @@
+from tkinter import *
+from tkinter import filedialog as fd
+from tkinter import messagebox as mb
+from tkinter import ttk
+import requests
+import pyperclip
+import json
+import os
+
+history_file = "upload_history."
+
+
+def save_history(filepath, link):
+    history = []
+    if os.path.exists(history_file):
+        with open(history_file, 'r') as f:
+            history = json.load(f)
+    history.append({"file_path": os.path.basename(filepath), "download_link": link})
+    with open(history_file, 'w') as f:
+        json.dump(history, f, indent=4)
+
+def upload():
+    try:
+        filepath = fd.askopenfilename()
+        if filepath:
+            with open(filepath, 'rb') as f:
+                files = {'file': f} # Чтение побайтно
+                response = requests.post('http://file.io', files=files)
+                response.raise_for_status()
+                link = response.json()['link']
+                entry.delete(0, END)
+                entry.insert(0, link)
+                pyperclip.copy(link)
+                save_history(filepath, link)
+                mb.showinfo("Ссылка скопирована", f"Ссылка {link} успешно скопирована в буфер обмена!")
+    except Exception as e:
+        mb.showerror("Ошибка!", f"Произошла ошибка: {e}")
+
+
+window = Tk()
+window.title("Сохранение файлов в облаке")
+window.geometry("400x200")
+
+button = ttk.Button(text="Загрузить файл", command=upload)
+button.pack()
+
+entry = ttk.Entry()
+entry.pack()
+
+window.mainloop()
