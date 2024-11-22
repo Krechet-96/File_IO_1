eb6ef6c1029a383193606f879d721af740402d83
Programmer <shakirov.ekb@gmail.com>
2024-11-22 22:23:36 +0500

    Добавлена обработка исключений.

---
+++ Урок_29_Денис_03_Добавляем_обработчик_искл..py
@@ -0,0 +1,33 @@
+from tkinter import *
+from tkinter import filedialog as fd
+from tkinter import messagebox as mb
+from tkinter import ttk
+import requests
+
+
+def upload():
+    try:
+        filepath = fd.askopenfilename()
+        if filepath:
+            with open(filepath, 'rb') as f:
+                files = {'file': f} # Чтение побайтно
+                response = requests.post('http://file.io', files=files)
+                response.raise_for_status()
+                link = response.json()['link']
+                entry.delete(0, END)
+                entry.insert(0, link)
+    except Exception as e:
+        mb.showerror("Ошибка!", f"Произошла ошибка: {e}")
+
+
+window = Tk()
+window.title("Сохранение файлов в облаке")
+window.geometry("400x200")
+
+button = ttk.Button(text="Загрузить файл", command=upload)
+button.pack()
+
+entry = ttk.Entry()
+entry.pack()
+
+window.mainloop()
